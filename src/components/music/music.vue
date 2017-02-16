779c20ae1b23bf4e8b70efdc1c9315bd6cf8900b
hhc <649775996@qq.com>
2017-02-16 18:12:44 +0800

    'addCode'

--- src/components/music/music.vue
+++ src/components/music/music.vue
@@ -1,12 +1,19 @@
 <template>
     <div id="music">
         <audio :src='src' ref="audio"></audio>
+        <md-input-container>
+            <label><md-icon>search</md-icon>搜索</label>
+            <md-input  @keyup.native.enter="search"></md-input>
+        </md-input-container>
+        <md-button class="md-raised md-primary random" @click="randomListen">
+            <md-icon>library_music</md-icon>随便听听
+        </md-button>
         <md-list>
-            <md-list-item v-for="(music,index) of musicList" @click="togglePlay(music)">
+            <md-list-item v-for="(music,index) of musicList" @click="togglePlay(music)" :class="{'odd':index%2}">
                 <md-avatar>
                     <img :src="music.album.picUrl">
                 </md-avatar>
-                <p>{{music.name}}/{{music.duration}}</p>
+                <p class="music-name">{{music.name}}/{{music.artists[0].name}}</p>
                 <md-button class="md-icon-button md-list-action">
                     <md-icon class="md-primary md-theme-teal">{{music.isPlay?'pause_circle_outline':'play_circle_outline'}}</md-icon>
                 </md-button>
@@ -26,7 +33,8 @@
             return{
                 musicList:[],
                 src:'',
-                snackContent:'网络异常，稍后再试.'
+                snackContent:'网络异常，稍后再试.',
+                playListId:600188621
             }
         },
         methods:{
@@ -39,19 +47,61 @@
                     this.$refs.audio.pause();
                     return;
                 }
-                this.src=music.mp3Url;
+                this.src=music.mp3Url?music.mp3Url:music.audio;
                 this.musicList.forEach(function (el) {
                     el.isPlay=false;
                 });
                 music.isPlay=true;
-
+                var me = this;
                 setTimeout(function(){
-                    this.$refs.audio.play().catch(function(e){
-                        this.snackContent="资源地址失效,请重新选择."
-                        this.openSnack();
+                    me.$refs.audio.play().catch(function(e){
+                        me.snackContent="资源地址失效,请重新选择."
+                        me.openSnack();
                         music.isPlay=true;
-                    }.bind(this));
-                }.bind(this),0);
+                    });
+                },50);
+            },
+            randomListen(){
+                this.playListId=parseInt(Math.random()*10*(60030000-20000000)+20000000);
+                this.getMusicList();
+            },
+            getMusicList(){
+                var me = this;
+                axios.get(API_PROXY+'http://music.163.com/api/playlist/detail?id='+me.playListId).then(function(res){
+                    if(res.status==200){
+                        me.musicList=[];
+                        if(!res.data.result.tracks.length){
+                            me.randomListen();
+                        }
+                        res.data.result.tracks.forEach(function(item){
+                            item.isPlay=false;
+                            me.musicList.push(item);
+                        });
+                    }else{
+                        this.snackContent="网络异常,刷新重试"
+                        me.openSnack();
+                    }
+                }).catch(function(error){
+                    console.log(error);
+                    me.randomListen();
+                });
+            },
+            search(e){
+                var me = this;
+                axios.get(API_PROXY+'http://s.music.163.com/search/get?type=1&s='+e.target.value).then(function(res){
+                    if(res.status==200){
+                        me.musicList=[];
+                        res.data.result.songs.forEach(function(item){
+                            item.isPlay=false;
+                            me.musicList.push(item);
+                        });
+                    }else{
+                        this.snackContent="网络异常,刷新重试"
+                        me.openSnack();
+                    }
+                }).catch(function(error){
+                    console.log(error);
+                });
             }
         },
         components:{
@@ -61,20 +111,7 @@
 
         },
         mounted:function(){
-            var me = this;
-            axios.get(API_PROXY+'http://music.163.com/api/playlist/detail?id=600188621').then(function(res){
-                if(res.status==200){
-                    res.data.result.tracks.forEach(function(item){
-                        item.isPlay=false;
-                        me.musicList.push(item);
-                    });
-                }else{
-                    this.snackContent="网络异常"
-                    me.openSnack();
-                }
-            }).catch(function(error){
-                console.log(error);
-            });
+            this.getMusicList();
         }
     }
 </script>
@@ -85,5 +122,29 @@
         box-sizing: border-box;
         padding: 64px 0;
         text-align: center;
+    }
+    .md-input-container{
+        margin: 0;
+        top: -5px;
+        label{
+            left: 16px;
+            top:20px;
+        }
+        input{
+            padding-left: 55px;
+        }
+    }
+    .random{
+        -webkit-transform:rotate(-7deg);
+        position: fixed;
+        top: 100px;
+        right: 0px;
+        z-index: 2;
+    }
+    .odd{
+        background: #f8f8f8;
+    }
+    .music-name{
+        white-space: nowrap; overflow: hidden;word-wrap:normal;-o-text-overflow:ellipsis;text-overflow:ellipsis;
     }
 </style>
