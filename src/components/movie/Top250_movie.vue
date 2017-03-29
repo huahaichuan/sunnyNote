bb29737b7f3b8a182eb11cefd1ed17376a241867
hhc <649775996@qq.com>
2017-03-29 18:14:57 +0800

    'complete'

---
+++ src/components/movie/Top250_movie.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="">
+
+    </div>
+</template>
+
+<script>
+    export default{
+        data()
+    {
+        return {}
+    }
+    ,
+    methods:{
+
+    }
+    ,
+    components:{
+
+    }
+    ,
+    computed:{
+
+    }
+    ,
+    mounted:function () {
+
+    }
+    }
+</script>
+
+<style lang="less">
+
+</style>
